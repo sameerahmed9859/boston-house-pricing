3e0d94d96d9cd45766b23c3a4c095efae3c1c25a
sameerahmed9859 <sameerahmed9859@gmail.com>
2023-02-18 15:54:24 +0530

    this is commit for web application

---
+++ app.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+import numpy as np
+from flask import Flask,app,jsonify,url_for,render_template,request
+from django.shortcuts import render
+
+app= Flask(__name__)
+
+# load the model 
+lrmodel=pickle.load(open('lrmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=lrmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__==__name__:
+    app.run(debug=True)
